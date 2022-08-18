ea2813eddcace259fba2fcfb0e7f7473912b46d0
Sebastian Krysmanski <noreply@manski.net>
2022-08-18 12:32:38 +0200

    Adds tests for RealFileSystem

---
+++ tests/Mustache.MSBuild.Tests/Tests/Utils/RealFileSystemTests.cs
@@ -0,0 +1,24 @@
+﻿using Mustache.MSBuild.Utils;
+
+using Shouldly;
+
+using Xunit;
+
+namespace Mustache.MSBuild.Tests.Utils;
+
+/// <summary>
+/// Tests for <see cref="RealFileSystem"/>.
+/// </summary>
+public sealed class RealFileSystemTests
+{
+    [Fact]
+    public void Test_Instance()
+    {
+        var assemblyPath = GetType().Assembly.Location;
+
+        var realFileSystem = RealFileSystem.Instance;
+
+        realFileSystem.File.Exists(assemblyPath).ShouldBe(true);
+        realFileSystem.File.Exists(assemblyPath + ".other").ShouldBe(false);
+    }
+}
