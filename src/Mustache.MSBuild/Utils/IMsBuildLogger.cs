8bc73bcedc24b9474a5b008c78998a793a835177
Sebastian Krysmanski <noreply@manski.net>
2022-09-02 07:12:50 +0200

    Cleanup IMsBuildLogger

--- src/Mustache.MSBuild/Utils/IMsBuildLogger.cs
+++ src/Mustache.MSBuild/Utils/IMsBuildLogger.cs
@@ -1,6 +1,8 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright Mustache.MSBuild (https://github.com/skrysmanski/Mustache.MSBuild)
 
+using JetBrains.Annotations;
+
 using Microsoft.Build.Utilities;
 
 namespace Mustache.MSBuild.Utils;
@@ -10,10 +12,17 @@ namespace Mustache.MSBuild.Utils;
 /// </summary>
 internal interface IMsBuildLogger
 {
+    /// <summary>
+    /// Logs the specified warning to the MSBuild output.
+    /// </summary>
+    [StringFormatMethod("message")]
     void LogWarning(string message, params object[] messageArgs);
 
     /// <summary>
-    /// This logging helper method is designed to capture and display information from arbitrary exceptions in a standard way.
+    /// Logs the specified exception to the MSBuild output.
     /// </summary>
+    /// <remarks>
+    /// This logging helper method is designed to capture and display information from arbitrary exceptions in a standard way.
+    /// </remarks>
     void LogErrorFromException(Exception exception);
 }
