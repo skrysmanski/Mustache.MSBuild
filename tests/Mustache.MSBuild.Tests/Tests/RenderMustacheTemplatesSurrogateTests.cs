ff70e7bdaa0791d5a5beb5b2e23391eb4bbc2349
Sebastian Krysmanski <noreply@manski.net>
2022-09-02 08:09:19 +0200

    Add test for existing files to RenderMustacheTemplatesSurrogateTests

--- tests/Mustache.MSBuild.Tests/Tests/RenderMustacheTemplatesSurrogateTests.cs
+++ tests/Mustache.MSBuild.Tests/Tests/RenderMustacheTemplatesSurrogateTests.cs
@@ -57,6 +57,61 @@ public sealed class RenderMustacheTemplatesSurrogateTests
         fileSystem.File.ReadAllText(outputFile).ShouldBe("<c>42</c>");
     }
 
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Test_Execute_ExistingFile(bool targetIsUpToDate)
+    {
+        // Setup
+        var fileSystem = new MockFileSystem();
+
+        const string TEMPLATE_CONTENTS = "<c>{{MyProperty}}</c>";
+        const string DATA_FILE_CONTENTS = "{ \"MyProperty\": 42 }";
+
+        var templateFile = MockUnixSupport.Path(@"C:\templates\MyFile.txt.mustache");
+        var dataFile = MockUnixSupport.Path(@"C:\templates\MyFile.txt.json");
+        var outputFile = MockUnixSupport.Path(@"C:\templates\MyFile.txt");
+
+        fileSystem.AddFile(templateFile, new MockFileData(TEMPLATE_CONTENTS));
+        fileSystem.AddFile(dataFile, new MockFileData(DATA_FILE_CONTENTS));
+
+        var originalLastWriteTime = DateTime.UtcNow - TimeSpan.FromHours(1);
+
+        fileSystem.AddFile(outputFile, new MockFileData(targetIsUpToDate ? "<c>42</c>" : "<c>XX</c>")
+        {
+            LastWriteTime = originalLastWriteTime,
+        });
+
+        var templatePaths = new[]
+        {
+            CreateMsBuildTaskItem(templateFile),
+        };
+
+        var logger = new MsBuildTestLogger();
+
+        var task = new RenderMustacheTemplatesSurrogate(fileSystem);
+
+        // Test
+        task.Execute(templatePaths, logger).ShouldBe(true);
+
+        // Verify
+        logger.Exceptions.ShouldBeEmpty();
+        logger.Warnings.ShouldBeEmpty();
+
+        fileSystem.File.Exists(outputFile);
+        fileSystem.File.ReadAllText(outputFile).ShouldBe("<c>42</c>");
+        if (targetIsUpToDate)
+        {
+            fileSystem.File.GetLastWriteTimeUtc(outputFile).ShouldBe(originalLastWriteTime);
+        }
+        else
+        {
+            // TODO: This requires this to be fixed: https://github.com/TestableIO/System.IO.Abstractions/issues/872
+            //fileSystem.File.GetLastWriteTimeUtc(outputFile).ShouldBeGreaterThan(originalLastWriteTime);
+            fileSystem.File.GetLastWriteTimeUtc(outputFile).ShouldNotBe(originalLastWriteTime);
+        }
+    }
+
     [Fact]
     public void Test_Execute_EmptyPaths()
     {
